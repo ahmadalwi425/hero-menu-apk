0a3698c52057cf12a17cfeeaeadf6cd4ce20a38d
AisyaChalvinaIzumi <1941720090@student.polinema.ac.id>
2021-12-27 21:22:00 +0700

    revisi_2

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key:key);
+
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
